5f9d01badd1f1c14e6fdcffefdcb2f824014b110
Pedro Henrique Schneider Conzatti <phconzatti@hotmail.com>
2025-07-22 13:05:25 -0300

    Corrigir imagens svg

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,22 +9,44 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text('Breakfast',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 18,
-          fontWeight: FontWeight.bold
-        )),
+        title: Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         elevation: 0,
         centerTitle: true,
         leading: Container(
-          width: 30,
-          height: 30,
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
           decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.circular(10)   
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left2.svg',
+            height: 20,
+            width: 20,
           ),
         ),
+        actions: [
+          Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/Arrow - Left2.svg',
+              height: 20,
+              width: 20,
+            ),
+          ),
+        ],
       ),
     );
   }
